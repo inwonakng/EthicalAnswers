a184b85ae74bf800a0b80e9ec6bdf49e04a1547f
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2021-05-15 12:32:38 +0530

    settings page

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import HomePage from "./pages/home/home"
+import registerPage from "./pages/register/register";
+import LoginPage from "./pages/login/login";
+
 import Surveys from "./pages/surveys/surveys"
-import registerPage from "./pages/register/register"
-import LoginPage from "./pages/login/login"
-import mySurveysPage from "./pages/mysurveys/mysurveys"
+import mySurveys from "./pages/mysurveys/mysurveys"
+import settingsPage from "./pages/settings/settings"
 import { Redirect, Route } from "react-router-dom";
 
 
 const BaseRouter = () => (
     <div>
         <Route exact path="/" render={(props) => <HomePage {...props}/>}/>
-        <Route exact path="/surveys" render={(props) => <Surveys {...props}/>}/>
-        <Route exact path="/mysurveys" component={mySurveysPage}/>
-
         <Route exact path="/register" component={registerPage}/>
         <Route exact path="/login" component={LoginPage}/>
+
+        <Route exact path="/surveys" render={(props) => <Surveys {...props}/>}/>
+        <Route exact path="/mysurveys" component={mySurveys}/>
+        <Route exact path="/settings" component={settingsPage}/>
+
     </div>
 )
 
