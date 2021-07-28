82590478114d637a69e7328e1c435481b1684d3d
chris32101 <48632502+chris32101@users.noreply.github.com>
2021-07-28 16:04:13 -0400

    Fixed background

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,29 +10,29 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='images/profile_pic.png'
               text='Inwon'
               label='Professor'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='images/profile_pic.png'
               text='Christopher Ng'
               label='Teaching Assistant'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='images/profile_pic.png'
               text='Alexander Montes'
               label='Mentor'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='images/profile_pic.png'
               text='Nishant Srivastava'
               label='Adventure'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='images/profile_pic.png'
               text='Megan Goulet'
               label='Student Lead'
             />
