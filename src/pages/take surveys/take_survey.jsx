722002253fdce3fb6426a4ea5b6e0eb207ed577a
chris32101 <48632502+chris32101@users.noreply.github.com>
2021-10-17 12:18:52 -0400

    Working

---
+++ src/pages/take surveys/take_survey.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./take_survey.css"
+import light_mode_logo from '../../components/logos/word_logo_black.png';
+
+import { Container, Form, Button, Card } from "react-bootstrap"
+import { toast } from 'react-toastify';
+import { connect } from "react-redux"
+import * as actions from "../../store/actions/auth"
+import { Link } from "react-router-dom";
+
+const Take_Survey = props => {
+    
+}
