d978db8c88353949113592654c7a761e3ed2ef43
Nishant Srivastava <nishi7409@gmail.com>
2021-10-29 16:30:27 -0400

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.css"
 // import light_mode_logo from '../../components/logos/word_logo_black.png';
 import Cards from '../../components/Cards';
-// import HeroSection from '../../components/HeroSection';
+import HeroSection from '../../components/HeroSection';
 // import Footer from '../../components/Footer';
 
 function homePage (props) {
