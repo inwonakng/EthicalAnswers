007ef69e2648f679c00e50dbdda75e29aca6a4f0
chris32101 <48632502+chris32101@users.noreply.github.com>
2021-11-07 21:11:39 -0500

    sliders

--- src/pages/take surveys/take_survey.jsx
+++ src/pages/take surveys/take_survey.jsx
@@ -3,11 +3,62 @@ import "./take_survey.css"
 import light_mode_logo from '../../components/logos/word_logo_black.png';
 
 import { Container, Form, Button, Card } from "react-bootstrap"
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 import { toast } from 'react-toastify';
 import { connect } from "react-redux"
 import * as actions from "../../store/actions/auth"
 import { Link } from "react-router-dom";
 
-const Take_Survey = props => {
-    
+function valuetext(value) {
+    return `${value}°C`;
 }
+
+export default function take_surveys (props) {
+    return(
+        <Box sx={{ width: 500 }}>
+            <Slider
+                aria-label="Option 1"
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={0}
+                max={10}
+            ></Slider>
+            <Slider
+                aria-label="Option 2"
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={0}
+                max={10}
+            ></Slider>
+            <Slider
+                aria-label="Option 3"
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={0}
+                max={10}
+            ></Slider>
+            <Slider
+                aria-label="Option 4"
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={0}
+                max={10}
+            ></Slider>
+        </Box>
+        
+    );   
+}
+
