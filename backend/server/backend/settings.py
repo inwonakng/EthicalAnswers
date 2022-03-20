71c3cd6f42a0b345156e9245f20b1a0461eae212
inwonakng <inwon.kang04@gmail.com>
2022-03-20 17:08:37 -0400

    engine??

--- backend/server/backend/settings.py
+++ backend/server/backend/settings.py
@@ -95,10 +95,10 @@ import os
 
 DATABASES = {
     'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     # 'NAME': os.environ.get('POSTGRES_NAME'),
-    #     # 'USER': os.environ.get('POSTGRES_USER'),
-    #     # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.environ.get('POSTGRES_NAME'),
+        # 'USER': os.environ.get('POSTGRES_USER'),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
     #     # 'HOST':'db',
     #     'PORT':'5432',
     #     'NAME': 'postgres',
